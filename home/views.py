644041a7e755080d3a97d4bc7d3d028afc47d412
Ashwin Mohan <ashwin.mohan@outlook.com>
2020-03-19 22:52:04 +0530

    secondcommit

--- home/views.py
+++ home/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse
 from django.contrib import messages
 
 from django.contrib.auth.models import User
-from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from home.models import url_shortener,code_shortener
+from home.models import url_shortener, code_shortener
 
-front_url='https://slcp.herokuapp.com/'
+front_url = 'https://slcp.herokuapp.com/'
+
 
 def signup(request):
     '''This function creates an account'''
@@ -26,26 +27,28 @@ def signup(request):
 
         if User.objects.filter(email=email).exists():
             return render(request, "home/signup.html", {'email_error': "Account with entered Email already exists!"})
-        
-        User.objects.create_user(username=username, email=email, password=password, first_name=fname, last_name=lname)
+
+        User.objects.create_user(username=username, email=email,
+                                 password=password, first_name=fname, last_name=lname)
         return redirect('home:login')
     else:
         return render(request, 'home/signup.html')
 
+
 def login_user(request):
     '''This function performs login of user'''
-    if(request.user.is_authenticated):                      #already logged in
+    if(request.user.is_authenticated):  # already logged in
         return redirect('home:dashboard')
-    
+
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-        if user is not None:            
+        if user is not None:
             login(request, user)
             return redirect('home:dashboard')
         else:
-            messages.error(request,'Invalid Username or Password')
+            messages.error(request, 'Invalid Username or Password')
             return render(request, 'home/login.html')
     else:
         return render(request, 'home/login.html')
@@ -53,7 +56,8 @@ def login_user(request):
 
 def landingpage(request):
     '''landing page if user isnt logged in'''
-    return render(request,'home/landingpage.html')
+    return render(request, 'home/landingpage.html')
+
 
 @login_required
 def logout_user(request):
@@ -61,10 +65,12 @@ def logout_user(request):
     logout(request)
     return redirect('home:login')
 
+
 @login_required
 def dashboard(request):
     '''This function displays the landing page'''
     return render(request, 'home/dashboard.html')
+
 
 def shorten_url_form(request):
     ''' This function receives URL and back-half from post method and add to database '''
@@ -75,19 +81,21 @@ def shorten_url_form(request):
         description = request.POST.get("description")
 
         if username != '':                                               # check username
-            try:                                                   
+            try:
                 User.objects.get(username=username)
             except:
                 return render(request, "home/url_form.html", {'username_error': "Entered username does not exist!"})
 
-        sobj = url_shortener.objects.all()                               # check back half already present
+        # check back half already present
+        sobj = url_shortener.objects.all()
         back_half_list = []
         for x in sobj:
             back_half_list.append(x.back_half)
         if back_half in back_half_list:
             return render(request, "home/url_form.html", {'backhalf_error': "Entered Back-Half already exists!"})
 
-        s = url_shortener()                                              # database entry
+        # database entry
+        s = url_shortener()
         s.url = url
         s.back_half = back_half
         s.description = description
@@ -103,11 +111,12 @@ def shorten_url_form(request):
 def redirect_to_url(request, key):
     '''This function receives back-half as parameter and redirects to URL stored in database'''
     try:
-        sobj = url_shortener.objects.get(back_half=key)         #select url from back-half
+        sobj = url_shortener.objects.get(
+            back_half=key)  # select url from back-half
         url = sobj.url
         return redirect(url)
     except:
-        return HttpResponse("<h1>ShortLink says:</h1><p>No such URL found</p>",content_type="html")
+        return HttpResponse("<h1>ShortLink says:</h1><p>No such URL found</p>", content_type="html")
 
 
 def shorten_code_form(request):
@@ -117,14 +126,22 @@ def shorten_code_form(request):
         code = request.POST.get("code")
         back_half = request.POST.get("backhalf")
         description = request.POST.get("description")
+        # check back half already present
+        sobj = code_shortener.objects.all()
+        back_half_list = []
+        for x in sobj:
+            back_half_list.append(x.back_half)
+        if back_half in back_half_list:
+            return render(request, "home/code_form.html", {'backhalf_error': "Entered Back-Half already exists!"})
 
         if username != '':                                               # check username
-            try:                                                   
+            try:
                 User.objects.get(username=username)
             except:
                 return render(request, "home/code_form.html", {'username_error': "Entered username does not exist!"})
 
-        c = code_shortener()                                              # database entry
+        # database entry
+        c = code_shortener()
         c.code = code
         c.back_half = back_half
         c.description = description
@@ -136,29 +153,32 @@ def shorten_code_form(request):
     else:
         return render(request, "home/code_form.html")
 
-def display_code(request,key):
+
+def display_code(request, key):
     '''This function receives back-half as parameter and displays your pasted code'''
     try:
         cobj = code_shortener.objects.get(back_half=key)
         code = cobj.code
-        return render(request, "home/display_code.html",{'code':code})
+        return render(request, "home/display_code.html", {'code': code})
     except:
-        return HttpResponse("<h1>ShortLink says:</h1><p>No such URL found</p>",content_type="html")
+        return HttpResponse("<h1>ShortLink says:</h1><p>No such URL found</p>", content_type="html")
+
 
 @login_required
 def display_all_codes(request):
     '''Display all short-code-links of the user'''
-    codes_list=[]
-    cobj=code_shortener.objects.filter(username=request.user)
+    codes_list = []
+    cobj = code_shortener.objects.filter(username=request.user)
     for i in cobj:
-        codes_list.append([i.description , front_url+'code/'+i.back_half+'/'])
-    return render(request, "home/display_codes.html",{'codes':codes_list})
+        codes_list.append([i.description, front_url+'code/'+i.back_half+'/'])
+    return render(request, "home/display_codes.html", {'codes': codes_list})
+
 
 @login_required
 def display_all_urls(request):
     '''Display all short-URLs of the user'''
-    urls_list=[]
-    sobj=url_shortener.objects.filter(username=request.user)
+    urls_list = []
+    sobj = url_shortener.objects.filter(username=request.user)
     for i in sobj:
-        urls_list.append([i.description, i.url , front_url+i.back_half+'/'])
-    return render(request, "home/display_urls.html",{'urls':urls_list})
+        urls_list.append([i.description, i.url, front_url+i.back_half+'/'])
+    return render(request, "home/display_urls.html", {'urls': urls_list})
