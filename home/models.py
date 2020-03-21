2d783479c6b4bc50d25858826d0361f54b3b9d64
Ashwin Mohan <ashwin.mohan@outlook.com>
2020-03-21 14:38:53 +0530

    refining of code

--- home/models.py
+++ home/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class url_shortener(models.Model):
     username = models.CharField(max_length=30, default='')
     url = models.URLField()
-    back_half = models.CharField(max_length=50)
-    description = models.TextField(default='',unique=True)
+    back_half = models.CharField(max_length=50,unique=True)
+    description = models.TextField(default='')
+    date_time = models.DateTimeField(default=datetime.now, blank=True)
 
 class code_shortener(models.Model):
     username = models.CharField(max_length=30, default='')
     code = models.TextField()
     back_half = models.CharField(max_length=50,unique=True)
     description = models.TextField(default='')
+    date_time = models.DateTimeField(default=datetime.now, blank=True)
 
 
--- home/views.py
+++ home/views.py
@@ -15,7 +15,8 @@ front_url = 'https://slcp.herokuapp.com/'
 
 def signup(request):
     '''This function creates an account'''
-    if request.method == 'POST':
+    
+    if request.method == 'POST':                                            #post method
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
@@ -28,157 +29,146 @@ def signup(request):
         if User.objects.filter(email=email).exists():
             return render(request, "home/signup.html", {'email_error': "Account with entered Email already exists!"})
 
-        User.objects.create_user(username=username, email=email,
-                                 password=password, first_name=fname, last_name=lname)
+        User.objects.create_user(username=username, email=email, password=password, first_name=fname, last_name=lname)
         return redirect('home:login')
-    else:
+   
+    else:                                                              #get method
         return render(request, 'home/signup.html')
 
 
 def login_user(request):
     '''This function performs login of user'''
-    if(request.user.is_authenticated):  # already logged in
+    
+    if(request.user.is_authenticated):                                          # already logged in
         return redirect('home:dashboard')
 
-    if request.method == "POST":
+    if request.method == "POST":                                                #post method
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
-            login(request, user)
+            login(request, user)                                        #add all user details to request for session 
             return redirect('home:dashboard')
         else:
             messages.error(request, 'Invalid Username or Password')
             return render(request, 'home/login.html')
-    else:
+   
+    else:                                               #get method
         return render(request, 'home/login.html')
 
 
 def landingpage(request):
     '''landing page if user isnt logged in'''
-    return render(request, 'home/landingpage.html')
+    
+    return render(request, 'home/landingpage.html')             # get display page
 
 
 @login_required
 def logout_user(request):
     '''This function logouts the user'''
-    logout(request)
+    
+    logout(request)                                                     #remove user details from request
     return redirect('home:login')
 
 
 @login_required
 def dashboard(request):
     '''This function displays the landing page'''
+    
     return render(request, 'home/dashboard.html')
 
 
 def shorten_url_form(request):
     ''' This function receives URL and back-half from post method and add to database '''
-    if request.method == "POST":
-        username = request.POST.get("username")
+   
+    if request.method == "POST":                                            #post method
+        username = request.POST.get("username") 
         url = request.POST.get("url")
         back_half = request.POST.get("backhalf")
         description = request.POST.get("description")
 
-        if username != '':                                               # check username
-            try:
-                User.objects.get(username=username)
-            except:
-                return render(request, "home/url_form.html", {'username_error': "Entered username does not exist!"})
+        if username != '' and not User.objects.filter(username=username).exists():         #if username is entered and username does not exist      
+            return render(request, "home/url_form.html", {'username_error': "Entered username does not exist!"})
 
-        # check back half already present
-        sobj = url_shortener.objects.all()
-        back_half_list = []
-        for x in sobj:
-            back_half_list.append(x.back_half)
-        if back_half in back_half_list:
+        if url_shortener.objects.filter(back_half=back_half).exists():                  # check back half exist 
             return render(request, "home/url_form.html", {'backhalf_error': "Entered Back-Half already exists!"})
-
-        # database entry
-        s = url_shortener()
-        s.url = url
-        s.back_half = back_half
-        s.description = description
-        s.username = username
-        s.save()
+        else:
+            s = url_shortener()
+            s.url = url
+            s.back_half = back_half
+            s.description = description
+            s.username = username
+            s.save()
 
         return_url = front_url+back_half+'/'
         return render(request, "home/url_form.html", {'url': return_url})
-    else:
+    
+    else:                                                                             #get method
         return render(request, "home/url_form.html")
 
 
 def redirect_to_url(request, key):
     '''This function receives back-half as parameter and redirects to URL stored in database'''
+   
     try:
-        sobj = url_shortener.objects.get(
-            back_half=key)  # select url from back-half
-        url = sobj.url
-        return redirect(url)
+        sobj = url_shortener.objects.get(back_half=key)                  # select url from back-half
+        return redirect(sobj.url)
+    
     except:
         return HttpResponse("<h1>ShortLink says:</h1><p>No such URL found</p>", content_type="html")
 
 
 def shorten_code_form(request):
     ''' This function receives code and back-half from post method and add to database '''
-    if request.method == "POST":
+   
+    if request.method == "POST":                                    #post method
         username = request.POST.get("username")
         code = request.POST.get("code")
         back_half = request.POST.get("backhalf")
         description = request.POST.get("description")
-        # check back half already present
-        sobj = code_shortener.objects.all()
-        back_half_list = []
-        for x in sobj:
-            back_half_list.append(x.back_half)
-        if back_half in back_half_list:
+
+        if username != '' and not User.objects.filter(username=username).exists():         #if username is entered and username does not exist      
+            return render(request, "home/code_form.html", {'username_error': "Entered username does not exist!"})
+       
+        if code_shortener.objects.filter(back_half=back_half).exists():                 #back-half exists check
             return render(request, "home/code_form.html", {'backhalf_error': "Entered Back-Half already exists!"})
-
-        if username != '':                                               # check username
-            try:
-                User.objects.get(username=username)
-            except:
-                return render(request, "home/code_form.html", {'username_error': "Entered username does not exist!"})
-
-        # database entry
-        c = code_shortener()
-        c.code = code
-        c.back_half = back_half
-        c.description = description
-        c.username = username
-        c.save()
+        else:
+           
+            c = code_shortener()                                                     # database entry
+            c.code = code
+            c.back_half = back_half
+            c.description = description
+            c.username = username
+            c.save()
 
         return_url = front_url+'code/'+back_half+'/'
         return render(request, "home/code_form.html", {'url': return_url})
-    else:
+   
+    else:                                                                    #Get method
         return render(request, "home/code_form.html")
 
 
 def display_code(request, key):
     '''This function receives back-half as parameter and displays your pasted code'''
+   
     try:
         cobj = code_shortener.objects.get(back_half=key)
-        code = cobj.code
-        return render(request, "home/display_code.html", {'code': code})
+        return render(request, "home/display_code.html", {'code': cobj.code})
     except:
         return HttpResponse("<h1>ShortLink says:</h1><p>No such URL found</p>", content_type="html")
 
 
 @login_required
 def display_all_codes(request):
-    '''Display all short-code-links of the user'''
-    codes_list = []
-    cobj = code_shortener.objects.filter(username=request.user)
-    for i in cobj:
-        codes_list.append([i.description, front_url+'code/'+i.back_half+'/'])
-    return render(request, "home/display_codes.html", {'codes': codes_list})
+    '''Display all short-code-links of the user logged in'''
+   
+    cobj = code_shortener.objects.filter(username=request.user).order_by('-date_time')                  #filter by username descending order
+    return render(request, "home/display_codes.html", {'codes': cobj, 'url' : front_url+'code/'})
 
 
 @login_required
 def display_all_urls(request):
-    '''Display all short-URLs of the user'''
-    urls_list = []
-    sobj = url_shortener.objects.filter(username=request.user)
-    for i in sobj:
-        urls_list.append([i.description, i.url, front_url+i.back_half+'/'])
-    return render(request, "home/display_urls.html", {'urls': urls_list})
+    '''Display all short-URLs of the user logged in'''
+   
+    uobj = url_shortener.objects.filter(username=request.user).order_by('-date_time')                    #filter by username descending order
+    return render(request, "home/display_urls.html", {'urls': uobj, 'url' : front_url})
