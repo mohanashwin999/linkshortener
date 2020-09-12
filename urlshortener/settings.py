823720360ca5754e3e501d0d95dda6b3997c5bc7
Ashwin  Mohan <mohanashwin999@gmail.com>
2020-09-12 20:03:53 +0530

    commit

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,4 +123,3 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
 
-django_heroku.settings(locals())
