b65251a9dc2ae923cb61cabb68af8e1c255a035f
Ashwin  Mohan <mohanashwin999@gmail.com>
2020-09-13 16:33:32 +0530

    changes in databases for deployment

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -75,14 +75,34 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
+DATABASES = {
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': ‘shortlink’,
+
+        'USER': 'ashwin',
+
+        'PASSWORD': 'unni8888',
+
+        'HOST': 'shortlink.comgskgglvgz.ap-south-1.rds.amazonaws.com',
+
+        'PORT': '5432',
+
+    }
+
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
